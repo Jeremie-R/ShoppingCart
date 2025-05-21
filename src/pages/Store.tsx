4604cccfc93312542779e91e2a8b964561a1d3f5
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2025-05-21 16:20:57 +0200

    built card component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,19 +1,22 @@
 import { Col, Row } from "react-bootstrap"
 import storeItems from "../data/items.json"
+import { StoreItem } from "../components/storeItem"
 
 
 
 export function Store() {
     return (
-        <div>
+        <>
         <h1>Store</h1>
 
-        <Row>
+        <Row md={2} xs={1} lg={3} className="g-3">
             {storeItems.map(item => (
-                <Col>{JSON.stringify(item)}</Col>
+                <Col key={item.id}>
+                    <StoreItem {...item} />
+                </Col>
             ))}
         </Row>
 
-        </div>
+        </>
     )
 }
